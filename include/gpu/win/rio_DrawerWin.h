66c2ba133e503095b393fbfb33365c6c040f2683
Arian Kordi <ariankordi@ariankordi.net>
2024-06-27 00:58:07 -0400

    Add hack to make DrawElements work on macOS
    A few things here.
    * For the record I don't undersatnd this, but Aboood called this a
    sh*tty solution.
    * I'm not sure whether this would make the drawing code work on other
    systems that don't support newer versions of OpenGL 4.x, I don't have
    anything else to test with.
    * Even on macOS, FFL-Testing's FFLShader looks... off. Almost like some
    sections are missing lighting? The basics are there though.

--- include/gpu/win/rio_DrawerWin.h
+++ include/gpu/win/rio_DrawerWin.h
@@ -50,7 +50,18 @@ inline void Drawer::DrawElements(PrimitiveMode mode, u32 count, const u32* indic
 
 inline void Drawer::DrawElements(PrimitiveMode mode, u32 count, const u16* indices)
 {
+#ifdef __APPLE__
+    // DrawElements function compatible with macOS
+    GLuint ebo;
+    RIO_GL_CALL(glGenBuffers(1, &ebo));
+    RIO_GL_CALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo));
+    RIO_GL_CALL(glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(u16), indices, GL_STATIC_DRAW));
+    RIO_GL_CALL(glDrawElements(mode, count, GL_UNSIGNED_SHORT, (void*)0));
+    RIO_GL_CALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, GL_NONE));
+    RIO_GL_CALL(glDeleteBuffers(1, &ebo));
+#else
     RIO_GL_CALL(glDrawElements(mode, count, GL_UNSIGNED_SHORT, indices));
+#endif
 }
 
 }
